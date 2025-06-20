37eaa1440634fd8c3b62b65f2a574a22bb5cd269
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-20 17:29:23 +0530

    Project page 😶‍🌫️😶‍🌫️

---
+++ pages/projects.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLayout>
+  <section class="w-full h-full">
+    <h1 class="w-full text-center font-cinzel-bold text-6xl relative">
+      <span class="absolute -top-7 opacity-60 text-white/20 left-[50%] -translate-x-[50%]" style="letter-spacing: 10px;">
+Works
+      </span>
+My Works
+  </h1>
+  </section>
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page-layout',
+})
+</script>
+
+<style>
+
+</style>
