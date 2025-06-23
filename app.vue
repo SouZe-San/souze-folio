d666216146d9031ef52bd8d674af5d2e4f68ca78
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-23 17:52:12 +0530

    🎢Gsap Plugins added

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
    <NuxtPage/>
   </div>
 </template>
+<script setup>
+import { gsap } from "gsap";
+import {SplitText} from 'gsap/SplitText'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
+import {ScrollSmoother} from 'gsap/ScrollSmoother'
+onBeforeMount(() => {
+  gsap.registerPlugin(SplitText,ScrollSmoother,ScrollTrigger)
+
+})
+</script>
