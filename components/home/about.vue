4eba2b5dada0dae932c3a86acf9f1b281bd9725e
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-24 23:07:29 +0530

    Animation Change

--- components/home/about.vue
+++ components/home/about.vue
@@ -22,26 +22,56 @@
         </div>
       </div>
 
-      <div class="md:text-3xl text-white/70 contentBody text">
-        <span class="sm:text-4xl text-white" style="letter-spacing: 0"> Hey There! </span>
-        I&#39;am
-        <div
-          id="owner"
-          class="text-white font-changa sm:text-[32px] relative overflow-hidden inline-block"
-          @mousemove="nameHover"
-          @mouseleave="scaleNormal"
-          ><span class="name-original">Soumyajit Mondal</span>
-          <span class="name-alt absolute flex top-0 left-0">Souze-san</span></div>, a Computer Science Graduate from India with a passion for clean code and wired Design.
-        Now, i Build as a
-        <span class="sm:text-[32px] font-changa text-white" 
-        @mousemove="smallScaleGrow"  @mouseleave="scaleNormal"
-        > Full-stack Developer </span>
-        who Loves turing ideas into interactive, scalable digital experience. Whether it&#39;s
-        front-end magic or back-end logic, I enjoy crafting thing that solve
-        <span class="sm:text-[32px] font-changa text-white" 
-        @mousemove="smallScaleGrow"  @mouseleave="scaleNormal"
-        > real problem </span> and spark
-        curiosity. Always exploring, always building - One line of code at a time.
+      <div class="relative"
+      @mouseenter="cursorHidden"
+      @mouseleave="scaleNormal" 
+      >
+        <div class="contentBody text md:text-3xl text-white/70">
+          <span class="sm:text-4xl text-white" style="letter-spacing: 0"> Hey There! </span>
+          I&#39;am
+          <span
+            id="owner"
+            class="text-white font-changa sm:text-[32px] relative"
+            >Soumyajit Mondal </span
+          >, a Computer Science Graduate from India with a passion for clean code and wired Design.
+          Now, i Build as a
+          <span
+            class="sm:text-[32px] font-changa text-white"
+          >
+            Full-stack Developer
+          </span>
+          who Loves turing ideas into interactive, scalable digital experience. Whether it&#39;s
+          front-end magic or back-end logic, I enjoy crafting thing that solve
+          <span
+            class="sm:text-[32px] font-changa text-white"
+          >
+            real problem
+          </span>
+          and spark curiosity. Always exploring, always building - One line of code at a time.
+        </div>
+
+        <!-- Hidden Content -->
+        <div class="fakeContent absolute md:text-3xl top-0 text-black/80" 
+        ref="maskBody"
+         @mousemove="clipGrow"
+          @mouseleave="clipHidden"
+        >
+          <span class="sm:text-4xl text-black" style="letter-spacing: 0"> Hey there! </span> My self
+          <span
+            class="text-black font-changa sm:text-[32px] under-hover"
+            style="letter-spacing: 2px; font-weight: 500;"
+            >Souze-san</span
+          >, a CS graduate from India with a passion for Wired Things ~_~. A full-stack developer
+          who&#39;s all about more than just writing code. It&#39;s about solving  <span
+            class="text-black font-changa sm:text-[32px] under-hover"
+            style="letter-spacing: 2px"
+            >real-world</span>
+          problems and creating digital experiences that matter. Whether it&#39;s designing smooth front-ends or <span
+            class="text-black font-changa sm:text-[32px] under-hover"
+            style="letter-spacing: 2px"
+            >debugging</span> back-end logic, I&#39;m passionate about building things that
+          work, scale, and spark curiosity.
+        </div>
       </div>
     </div>
   </section>
@@ -49,34 +79,79 @@
 
 <script lang="ts" setup>
 import { SplitText } from "gsap/SplitText";
-
-const { setHover } = useHover();
 import gsap from "gsap";
 
-let hoverTimeline :gsap.core.Timeline
+const { setHover } = useHover();
+const { cursorPosition } = useCursorPosition();
+
+const size = ref(0);
+const maskBody = ref();
+const rafId = ref<number | null>(null);
+const isOver = ref(false);
+
+const updateMousePosition = (e: MouseEvent) => {
+  console.log(isOver.value)
+  size.value = computed(() => (isOver.value ? 200 : 0)).value;
+};
 
 function scaleGrow() {
   setHover(true);
 }
-function smallScaleGrow() {
-  setHover(true, "small");
-}
-function nameHover() {
-  setHover(true, "small");
-  hoverTimeline.play()
+function cursorHidden() {
+  setHover(true, "none");
 }
 
 function scaleNormal() {
   setHover(false);
-  hoverTimeline.reverse()
+  size.value = 0
 }
 
+function clipGrow() {
+  isOver.value = true;
+}
+function clipHidden() {
+  isOver.value = false;
+}
+
+
+// Mask Animation
+const moveMask = (x: number, y: number, size: number) => {
+  if (!maskBody.value) return;
+
+  gsap.set(maskBody.value, {
+    maskPosition: `${x - size / 2}px ${y - size / 2}px`,
+    ease: "back.out",
+  });
+  gsap.set(maskBody.value, {
+    maskSize: size,
+    ease:"power3.inOut"
+  });
+
+};
+
+
+const animate = () => {
+  if (!maskBody.value) return;
+  const { x, y } = cursorPosition.value;
+  const { x:boxX,y: boxY } = maskBody.value.getBoundingClientRect();
+
+  moveMask(x-boxX, y - boxY, size.value);
+
+  rafId.value = window?.requestAnimationFrame(animate);
+};
+
+
+watchEffect((onCleanup) => {
+  animate();
+  maskBody.value?.addEventListener("mousemove", updateMousePosition);
+
+  onCleanup(() => {
+    maskBody.value?.removeEventListener("mousemove", updateMousePosition);
+    window?.cancelAnimationFrame(rafId.value!);
+  });
+});
+
 onMounted(() => {
-  const originalSplit = SplitText.create(".name-original", { type: "chars" });
-  const altSplit = SplitText.create(".name-alt", { type: "chars" });
-
-   hoverTimeline = gsap.timeline({paused:true})
-
   const triggerTl = gsap.timeline({
     scrollTrigger: {
       trigger: ".aboutSection",
@@ -104,7 +179,6 @@ onMounted(() => {
         self.chars,
         {
           y: 100,
-         
         },
         {
           y: 0,
@@ -138,7 +212,7 @@ onMounted(() => {
       ease: "power2.out",
       scrollTrigger: {
         trigger: ".aboutSection",
-        start: "top center",
+        start: "top center-=50",
         end: "top center+=100",
 
         toggleActions: "play none reverse reverse",
@@ -146,7 +220,7 @@ onMounted(() => {
     }
   );
 
-  let content = SplitText.create(".text", {
+  SplitText.create(".text", {
     type: "chars,words",
     autoSplit: true,
     onSplit(self) {
@@ -161,45 +235,14 @@ onMounted(() => {
           stagger: 0.05,
           scrollTrigger: {
             trigger: ".aboutSection",
-            start: "center-=150 center",
-            end: "center+=100 center+=50",
+            start: "center-=200 center+=100",
+            end: "center+=100 center+=70",
             scrub: true,
-            //  toggleActions: "play none none reverse",
           },
         }
       );
     },
   });
-
-
-//   hover animations 
-
-    hoverTimeline.fromTo(originalSplit.chars,{
-      y:0,
-   autoAlpha:1
-    } ,{
-      y: -40,
-     autoAlpha:.5,
-      duration: 0.3,
-      ease: "power2.out",
-      stagger: 0.05
-    }, 0)
-    .fromTo(altSplit.chars, 
-      {
-        y: 40,
-        autoAlpha: 0
-      },
-      {
-        y: 0,
-        autoAlpha: 1,
-        duration: 0.3,
-        ease: "power2.out",
-        stagger: 0.05
-      },
-    0);
-
-
-
 });
 </script>
 
@@ -213,33 +256,42 @@ onMounted(() => {
   background: rgba(255, 255, 255, 0.828);
 }
 
-.contentBody {
+.contentBody,
+.fakeContent {
   line-height: 1.5;
   letter-spacing: 1px;
   word-wrap: break-word;
 }
 
-#owner {
- line-height: .97;
+.fakeContent{
+  background: #ffffff;
+  padding: 5px 0;
+ mask-image: url("/public/assets/mask.svg");
+  mask-repeat: no-repeat;
+  mask-size: 0px;
+}
+
+.under-hover {
+  position: relative;
   &::after {
     content: "";
     position: absolute;
-    bottom:0;
+    bottom: 10px;
     left: 0;
     height: 1px;
     width: 0%;
-    background: whitesmoke;
-    transition: width 0.5s ease-in-out;
+    background: rgb(0, 0, 0);
+    transition: width 0.3s ease-in-out;
   }
 
   &:hover {
     &::after {
-      width: 70%;
+      width: 100%;
     }
   }
 }
 
-.name-alt{
-   letter-spacing: 3px;
+.name-alt {
+  letter-spacing: 3px;
 }
 </style>
