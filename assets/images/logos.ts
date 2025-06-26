7944265aba04ef4d1a99dc614c3e7b2bfcde75fc
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-27 00:03:22 +0530

    Assets Distribution

---
+++ assets/images/logos.ts
@@ -0,0 +1,62 @@
+import bun from "~/public/assets/logos/logo-bun.webp";
+import deno from "~/public/assets/logos/logo-deno.svg";
+import docker from "~/public/assets/logos/logodocker.png";
+import express from "~/public/assets/logos/logo-expressjs.svg";
+import fresh from "~/public/assets/logos/logo-fresh.svg";
+import hono from "~/public/assets/logos/logo-hono.svg";
+import mongoDB from "~/public/assets/logos/logo-mongodb.svg";
+import next from "~/public/assets/logos/logo-next.webp";
+import node from "~/public/assets/logos/logo-nodejs.svg";
+import postgres from "~/public/assets/logos/logo-postgress.webp";
+import prisma from "~/public/assets/logos/logoprisma.webp";
+import reactJS from "~/public/assets/logos/logo-react.svg";
+import redux from "~/public/assets/logos/logo-redux.webp";
+import scss from "~/public/assets/logos/logo-sass.svg";
+import supabase from "~/public/assets/logos/logo-supabase.svg";
+import sanity from "~/public/assets/logos/logo-sanity.svg";
+import tailwind from "~/public/assets/logos/logo-tailwindcss.svg";
+import threeJs from "~/public/assets/logos/logo-three-js.webp";
+import vue from "~/public/assets/logos/logo-vue.webp";
+
+export type TechIconType =
+  | "bun"
+  | "deno"
+  | "docker"
+  | "express"
+  | "fresh"
+  | "hono"
+  | "postgres"
+  | "prisma"
+  | "redux"
+  | "scss"
+  | "supabase"
+  | "next"
+  | "reactJS"
+  | "node"
+  | "mongoDB"
+  | "sanity"
+  | "threeJs"
+  | "tailwind"
+  | "vue";
+
+export const techIcons = {
+  bun,
+  deno,
+  docker,
+  express,
+  fresh,
+  hono,
+  postgres,
+  prisma,
+  redux,
+  scss,
+  supabase,
+  next,
+  reactJS,
+  node,
+  mongoDB,
+  sanity,
+  threeJs,
+  tailwind,
+  vue,
+};
