e6b161a9f818ad843055fe6958cf1b6f10ebdfbc
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-27 16:14:30 +0530

    Remove all Odorkari

--- app.vue
+++ app.vue
@@ -1,24 +1,10 @@
 <template>
-  <NuxtLayout >
-  
-        <NuxtPage />
- 
+  <NuxtLayout>
+    <NuxtPage />
   </NuxtLayout>
 </template>
 <script setup lang="ts">
-import { gsap } from "gsap";
-import { SplitText } from "gsap/SplitText";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-import { CustomEase } from "gsap/CustomEase";
-
-
-onBeforeMount(() => {
-  gsap.registerPlugin(SplitText, ScrollSmoother, ScrollTrigger, CustomEase);
-  
-});
+onBeforeMount(() => {});
 </script>
 
-<style >
-
-</style>
+<style></style>
