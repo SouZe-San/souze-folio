62b412d86406363ae00ac55d2ffd94e663c559f2
SouZe-San <103335953+SouZe-San@users.noreply.github.com>
2025-06-28 20:06:04 +0530

    Project Now Gonna

--- components/projects/item-row.vue
+++ components/projects/item-row.vue
@@ -4,68 +4,77 @@
     @mouseenter="(e) => manageMouseEnter(e)"
     @mouseleave="(e) => manageMouseLeave(e)"
   >
-  <div class="py-4 px-32 inside">
-    <div class="flex w-full gap-1 justify-between items-end">
-      <div
-        class="flex items-center  lg:text-[8rem] md:text-5xl text-md font-semibold item-head"
-        @mousemove="scaleUp"
-        @mouseleave="scaleNormal"
-      >
-        <h1 class="part-1"><span class="font-cinzel-bold">I</span>mage</h1>
-        <div class="img">
-          <img :src="img" alt="bg-img" />
-        </div>
-        <h1 class="part-2">
-          <span class="font-cinzel-regular">Morph</span>
-        </h1>
-      </div>
-      <div class="flex gap-3" @mousemove="scaleMidUp" @mouseleave="scaleNormal">
-        <IconsSocialsGithubLink class="w-10" />
-        <NuxtLink to="https://github.com" class="flex items-center gap-1">
-          <IconsArrow
-            class="w-7 ml-3 text-white opacity-60 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
-        /></NuxtLink>
-      </div>
-    </div>
-
-    <div class="flex flex-col gap-2 mt-2">
-      <p class="md:text-lg text-sm description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, obcaecati.
-      </p>
-      <div class="flex justify-between items-center gap-8 md:gap-14">
+    <div class="py-4 px-32 inside">
+      <div class="flex w-full gap-1 justify-between items-end">
         <div
-          class="flex gap-2 overflow-x-scroll no-scrollbar"
-          @mousemove="scaleMidUp"
+          class="flex items-center 2xl:text-[8rem] lg:text-[6.5rem] md:text-5xl text-md font-semibold item-head"
+          @mousemove="scaleUp"
           @mouseleave="scaleNormal"
         >
-          <span
-            v-for="(tech, index) in techStack"
-            class="mt-1 badge capitalize border border-white/40 rounded-xl w-auto blur-bg"
-            :key="index.toString()"
-          >
-            <img
-              :src="techIcons[tech]"
-              :alt="`${tech} logo used in title`"
-              class="w-5 object-contain"
-              loading="lazy"
-            />
-
-            {{ tech }}
-          </span>
+          <h1 class="part-1"><span class="font-cinzel-bold">{{ title.first.charAt(0) }}</span>{{ title.first.slice(1) }}</h1>
+          <div class="img">
+            <img :src="imagUrl" alt="bg-img" />
+          </div>
+          <h1 class="part-2">
+            <span class="font-cinzel-regular">{{ title.last }}</span>
+          </h1>
         </div>
-        <div class="text-xl">
-          <span class="font-cinzel-bold"> June,</span><span class="font-cinzel-regular"> 2025</span>
+        <div class="flex flex-col justify-between h-[6rem]">
+          <div class="project-type font-changa uppercase">{{ projectType }}</div>
+          <div class="flex gap-3" @mousemove="scaleMidUp" @mouseleave="scaleNormal">
+            <NuxtLink :to="gitUrl" target="_blank" class="flex items-center gap-1 link">
+            <IconsSocialsGithubLink class="w-10" />
+            </NuxtLink>
+            <NuxtLink v-if="liveUrl" :to="liveUrl" target="_blank" class="flex items-center gap-1 link">
+              <IconsArrow
+                class="w-7 ml-3 text-white opacity-60 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+            /></NuxtLink>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div class="flex flex-col gap-2 mt-2">
+        <p class="md:text-lg text-sm description">
+         {{ description }}
+        </p>
+        <div class="flex justify-between items-center gap-8 md:gap-14">
+          <div
+            class="flex gap-2 overflow-x-scroll no-scrollbar tech-stack-section"
+            @mousemove="scaleMidUp"
+            @mouseleave="scaleNormal"
+
+          >
+            <span
+              v-for="(tech, index) in techStack"
+              class="mt-1 badge capitalize border border-white/40 rounded-xl w-auto blur-bg"
+              :key="index.toString()"
+            >
+              <img
+                :src="techIcons[tech]"
+                :alt="`${tech} logo used in title`"
+                class="w-5 aspect-square object-contain"
+                loading="lazy"
+              />
+
+              {{ techName[tech] }}
+            </span>
+          </div>
+          <div class="text-xl">
+            <span class="font-cinzel-bold"> {{ date.month }},</span
+            ><span class="font-cinzel-regular"> {{ date.year }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { type TechIconType, techIcons } from "~/assets/images/logos";
+import { techIcons } from "~/assets/images/logos";
+import type { Project } from "./projectsData";
+
 const { setHover } = useHover();
-const {$gsap} = useNuxtApp()
+const { $gsap } = useNuxtApp();
 
 function scaleUp() {
   setHover(true, "big");
@@ -78,57 +87,99 @@ function scaleNormal() {
   setHover(false);
 }
 
-const techStack: TechIconType[] = [
-  "next",
-  "redux",
-  "node",
-  "express",
-  "prisma",
-  "postgres",
-  "docker",
-];
 
-import img from "~/assets/images/projects/002.png";
+defineProps<Project>()
+const techName = {
+  bun :"Bun",
+  cssAnimation: "Css Animation",
+  deno:"Deno",
+  docker : "Docker",
+  express :"express Js",
+  fresh :"Fresh Js",
+  gsap :"GSAP",
+  hono :"Hono Js",
+  postgres : "PostgresSql",
+  prisma:"Prisma ",
+  redux :"Redux",
+  scss :"scss",
+  supabase :"supabase",
+  stability:"Stability AI",
+  next : "Next Js",
+  reactJS : "ReactJs",
+  node : "Node",
+  mongoDB : "MongoDb",
+  sanity :"sanity",
+  threeJs :"Three.js",
+  tailwind : "Tailwind CSS",
+  ts :"TypeScript",
+  vue :"VueJs",
+};
 
 const manageMouseEnter = (e: MouseEvent) => {
-  $gsap.to(e.target, { top: "-6vw",background:'linear-gradient(45deg,rgba(54, 54, 54, 1) 0%, rgba(31, 30, 30, 1) 34%, rgba(31, 28, 28, 0.97) 63%, rgba(59, 55, 55, 1) 94%)', duration: 0.3, ease: "power2.inOut" });
+  $gsap.to(e.target, {
+    top: "-6rem",
+    background:
+      "linear-gradient(45deg,rgba(54, 54, 54, 1) 0%, rgba(31, 30, 30, 1) 34%, rgba(31, 28, 28, 0.97) 63%, rgba(59, 55, 55, 1) 94%)",
+    duration: 0.4,
+    ease: "power2.inOut",
+  });
 };
 
 const manageMouseLeave = (e: MouseEvent) => {
-  $gsap.to(e.target, { top: "0",background:'#151515', duration: 0.3, delay: 0.1});
+  $gsap.to(e.target, { top: "0", background: "#151515", duration: 0.3, delay: 0.1 });
 };
 </script>
 
 <style scoped>
 .row-item {
   position: relative;
-  margin-bottom: -6vw;
-  background:#151515 ;
- 
+  margin-bottom: -6rem;
+  background: #151515;
+
   border-top: 1px solid rgba(200, 199, 199, 0.52);
 }
-.inside{
+.inside {
   background: url("/assets/images/background/noisy-2.png");
 }
 .description {
   line-height: 1;
   letter-spacing: 1px;
   word-spacing: 2px;
-  /* font-size: 18px; */
+
 }
 
 .blur-bg {
-  background: url(/assets/images/background/noisy-2.png), #eeeeee13;
+  background: url("/public/noiseTex.webp"), #eeeeee13;
+  background-size: 180px;
+ 
   backdrop-filter: blur(1rem);
 }
 
 .badge {
   display: flex;
+  align-items: center;
   gap: 5px;
-  padding: 4px 25px;
+  padding: 4px 20px;
   border: 2px solid #d1d1d157;
   border-radius: 25px;
   letter-spacing: 2px;
+  flex-shrink: 0;
+  color: #f4f4f4d4;
+}
+.project-type {
+  font-size: 14px;
+  opacity: 0.5;
+  letter-spacing: 4px;
+  border: 2px solid #dfdfdf4f;
+  padding: 0px 6px;
+}
+
+.tech-stack-section{
+   -ms-overflow-style: none; 
+    scrollbar-width: none; 
+    -webkit-overflow-scrolling: touch; 
+  
+
 }
 
 .item-head {
@@ -150,6 +201,26 @@ const manageMouseLeave = (e: MouseEvent) => {
   &:hover {
     .img {
       flex: 1;
+    }
+  }
+}
+
+a.link {
+  position: relative;
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: -3px;
+    left: 5px;
+    width: 0%;
+    height: 2px;
+    background: white;
+    transition: width 0.4s ease-in-out;
+  }
+
+  &:hover {
+    &::after {
+      width: 100%;
     }
   }
 }
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,51 +1,74 @@
 <template>
   <main>
-    <NavPageHeader/>
+    <NavPageHeader />
     <div class="back_white_blob_top"></div>
-      <div class="back_white_blob_down"></div>
- 
-  <section class="w-full h-full flex flex-col items-center">
-    <div class="w-full text-center mb-20">
+    <div class="back_white_blob_down"></div>
 
-      <h1 class="inline font-cinzel-bold sm:text-6xl text-[40px] max-sm:mt-4 relative" 
-       @mousemove="scaleGrow"
-          @mouseleave="scaleNormal">
-        <span class="absolute sm:-top-7 -top-5 opacity-60 text-white/20 left-[50%] -translate-x-[50%]" style="letter-spacing: 10px;">
-  Works
-        </span>
-  My Works
-    </h1>
-    </div>
-      
-    <!-- <ProjectsItem/> -->
+    <section class="w-full h-full flex flex-col gap-24 items-center">
+      <div class="w-full text-center mb-20">
+        <h1
+          class="inline font-cinzel-bold sm:text-6xl text-[40px] max-sm:mt-4 relative"
+          @mousemove="scaleGrow"
+          @mouseleave="scaleNormal"
+        >
+          <span
+            class="absolute sm:-top-7 -top-5 opacity-60 text-white/20 left-[50%] -translate-x-[50%]"
+            style="letter-spacing: 10px"
+          >
+            Works
+          </span>
+          My Works
+        </h1>
+      </div>
 
-    <div class="relative w-full mt-24 mb-30">
-      <ProjectsItemRow/>
-      <ProjectsItemRow/>
-      <ProjectsItemRow/>
-      <ProjectsItemRow/>
-    </div>
-  </section>
-   </main>
+      <div class="relative w-full mb-30">
+        <ProjectsItemRow
+          v-for="(project, index) in AllProjects"
+          :key="index.toString()"
+          v-bind="project"
+        />
+      </div>
+
+      <div class="mb-12 flex justify-center items-center w-full font-changa">
+        <button>
+          View More of Them
+          <IconsArrow class="w-4" />
+        </button>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
+import { AllProjects } from "~/components/projects/projectsData";
 
 const { setHover } = useHover();
-
-
-function scaleGrow(){
-  setHover(true)
+function scaleGrow() {
+  setHover(true);
 }
-function scaleMidGrow(){
-  setHover(true)
+function scaleMidGrow() {
+  setHover(true);
 }
 
-function scaleNormal(){
-setHover(false)
+function scaleNormal() {
+  setHover(false);
 }
 </script>
 
-<style>
-
+<style scoped>
+button {
+  border: 1px solid #cfcfcf87;
+  margin-top: 1rem;
+  font-size: 24px;
+  border-radius: 30px;
+  padding: 5px 24px;
+  display: flex;
+  gap: 10px;
+  font-weight: 400;
+  background: url("/public/noiseTex.webp") repeat, #eeeeee13;
+  background-size: 200px;
+  backdrop-filter: blur(1rem);
+  color: #d8a5e6;
+  box-shadow: 0 7px 6px -1px #00000073, 0 3px 63px -3px #ffffff2b;
+}
 </style>
