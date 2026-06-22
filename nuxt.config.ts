1c9ef7d13daa55ca84a6878cd41be7b991432a65
souze-san <2003soumyajit@gmail.com>
2026-06-22 19:12:11 +0530

    Package update : nuxt/image add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,11 +20,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-
+  modules: ["@nuxt/image"],
   vite: {
     plugins: [tailwindcss()],
   },
   build: {
     transpile: ["gsap"],
   },
+
 });
